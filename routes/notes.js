678a79bfb6a958c77d52b61a7eff40ebac36a1d0
leeclaire156 <lee.claire156@gmail.com>
2023-01-29 19:52:29 -0500

    Receives API POST request

--- routes/notes.js
+++ routes/notes.js
@@ -5,18 +5,20 @@ const notes = require('express').Router(); //allows for routes/index.js to redir
 // TODO: Create GET route to read the data on one of the notes
 
 notes.get('/', (req, res) => {
-    console.info(`${req.method} request received!`)
+    console.info(`${req.method} request received!`);
 
 });
 
 // TODO: Create POST route to show the data from the note selected and display it on Notes HTML page
-app.post('/', (req, res) => {
-
+notes.post('/', (req, res) => {
+    console.info(`${req.method} request received!`);
+    
 });
 
 
-// DELETE Route to delete the data from notes - - note that line 47 of index.js has a path called `api/notes/${id}` so we will be using '/api/notes/:id'
-// app.delete('/:id', (req, res) => {
+
+// // DELETE Route to delete the data from notes - - note that line 47 of index.js has a path called `api/notes/${id}` so we will be using '/api/notes/:id'
+// notes.delete('/:id', (req, res) => {
 //     const noteID = req.params.id;
 
 //     //Filter out id from existing notes
